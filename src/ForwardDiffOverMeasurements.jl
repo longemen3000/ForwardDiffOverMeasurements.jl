c8d5908bb81fcdfa19907702a065f27facfe48e6
longemen3000 <longemen3000@gmail.com>
2022-03-10 22:20:42 -0300

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.24

---
+++ src/ForwardDiffOverMeasurements.jl
@@ -0,0 +1,5 @@
+module ForwardDiffOverMeasurements
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using ForwardDiffOverMeasurements
+using Test
+
+@testset "ForwardDiffOverMeasurements.jl" begin
+    # Write your tests here.
+end
