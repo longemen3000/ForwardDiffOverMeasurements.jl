eb3838d530dbcf8f7cf49c4a38639663a7f4c0b3
longemen3000 <longemen3000@gmail.com>
2023-08-07 01:55:39 -0400

    overload all binary functions, using DiffRules instead of hardcoded approach

--- src/ForwardDiffOverMeasurements.jl
+++ src/ForwardDiffOverMeasurements.jl
@@ -1,6 +1,6 @@
 module ForwardDiffOverMeasurements
 
-using ForwardDiff: Dual
+using ForwardDiff: Dual, DiffRules, NaNMath, LogExpFunctions, SpecialFunctions
 using Measurements: Measurement
 import Base: +,-,/,*,promote_rule 
 
@@ -13,33 +13,35 @@ function promote_rule(::Type{Measurement{V1}}, ::Type{Dual{T, V2, N}}) where {V1
     return Dual{T , Vx, N}
 end
 
-macro overload_ambiguous(op)
+function overload_ambiguous_binary(M,f)
+    Mf = :($M.$f)
     return quote
-        @inline function $op(x::Dual{Tx,V}, y::Measurement{V}) where {Tx,V}
-            y = Dual{Tx}(y)
-            $op(x,y)
+        @inline function $Mf(x::Dual{Tx}, y::Measurement) where {Tx}
+            ∂y = Dual{Tx}(y)
+            $Mf(x,∂y)
         end
         
-        @inline function $op(x::Measurement{V},y::Dual{Ty,V}) where {V,Ty}
-            x = Dual{Ty}(x)
-            $op(x,y)
+        @inline function $Mf(x::Measurement,y::Dual{Ty}) where {Ty}
+            ∂x = Dual{Ty}(x)
+            $Mf(∂x,y)
         end
-        
-        @inline function $op(x::Dual{Tx}, y::Measurement) where {Tx}
-            y = Dual{Tx}(y)
-            $op(x,y)
-        end
-        
-        @inline function $op(x::Measurement,y::Dual{Ty}) where {Ty}
-            x = Dual{Ty}(x)
-            $op(x,y)
-        end
-    end |> esc
+    end
 end
 
-@overload_ambiguous Base.:+
-@overload_ambiguous Base.:-
-@overload_ambiguous Base.:*
-@overload_ambiguous Base.:/
+#use DiffRules.jl rules
+
+for (M, f, arity) in DiffRules.diffrules(filter_modules = nothing)
+    if (M, f) in ((:Base, :^), (:NaNMath, :pow))
+        continue  # Skip methods which we define elsewhere.
+    elseif !(isdefined(@__MODULE__, M) && isdefined(getfield(@__MODULE__, M), f))
+        continue  # Skip rules for methods not defined in the current scope
+    end
+    if arity == 2
+        eval(overload_ambiguous_binary(M,f))
+    else
+        # error("ForwardDiff currently only knows how to autogenerate Dual definitions for unary and binary functions.")
+        # However, the presence of N-ary rules need not cause any problems here, they can simply be ignored.
+    end
+end
 
 end #module
