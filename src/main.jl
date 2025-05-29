1cebbdc4f1bf2935e1f7c21817a71d35a24c10c5
longemen3000 <longemen3000@gmail.com>
2025-05-29 01:30:37 -0400

    more promote_rules

--- src/main.jl
+++ src/main.jl
@@ -62,6 +62,20 @@ function promote_rule(::Type{Measurement{V1}}, ::Type{Dual{T, Measurement{V2}, N
     return Dual{T, Vx, N}
 end
 
+function promote_rule(::Type{Dual{T, V, N}}, ::Type{Measurement{V}}) where {T,V,N}
+    Dual{T, Measurement{V}, N}
+end
+
+function promote_rule( ::Type{Dual{T, V2, N}}, ::Type{Measurement{V1}}) where {T, V2, N, V1<:AbstractFloat}
+    Vx = Measurement{promote_type(V1,V2)}
+    return Dual{T, Vx, N}
+end
+
+function promote_rule(::Type{Dual{T, Measurement{V2}, N}}, ::Type{Measurement{V1}}) where {T, V2<:AbstractFloat, N, V1<:AbstractFloat}
+    Vx = Measurement{promote_type(V1,V2)}
+    return Dual{T, Vx, N}
+end
+
 
 
 #=
